04870602f4a9a8d25bc7266cfcca3eac482a21dc
Arfaoui-ghaith <53495319+Arfaoui-ghaith@users.noreply.github.com>
2022-04-26 06:16:03 +0200

    push

--- imdb/main.py
+++ imdb/main.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_restful import Resource, Api
 import os
 import json
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
